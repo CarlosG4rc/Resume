823f448048c896e52502ee006228be10ea644735
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2022-11-08 14:49:53 -0600

    Add files via upload

---
+++ py/udi.py
@@ -0,0 +1,11 @@
+# print('Hello World')
+import requests
+
+url = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP68257/datos/2022-11-01/2022-11-02?token=04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
