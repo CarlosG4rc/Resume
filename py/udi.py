20245eed4b984e153cde1edee0f99a05d8f67509
Carlos García <carlosesolrac93@gmail.com>
2022-11-11 12:00:06 -0600

    Update udi.py

--- py/udi.py
+++ py/udi.py
@@ -1,17 +1,18 @@
 # print('Hello World')
 import requests
-import json 
+import urllib.parse
 
 url = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP68257/datos/2022-11-01/2022-11-02"
+url = url + urllib.parse.urlparse({"token":"04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471"})
+
+print(url)
 
 payload={}
 headers = {}
-parametres = {
-    "token": "04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471"
-}
+# parametres = {
+#     "token": "04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471"
+# }
 
-response = requests.get(url, params=parametres)
+response = requests.get(url)
 
-text = json.dumps(response.text, sort_keys=True, indent=4)
-
-print(text)
+print(response)
