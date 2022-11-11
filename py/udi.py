0e46e77e2dcf718545f56d2815f3f7d00da8fbf0
Carlos García <carlosesolrac93@gmail.com>
2022-11-11 13:55:17 -0600

    Update udi.py

--- py/udi.py
+++ py/udi.py
@@ -3,7 +3,7 @@ import math
 def areaCirculo(radio):
     return (radio**2) * math.pi
 
-r = input ("Cuál es el radio del circulo? " )
+r = 2
 r = int(r)
 a = areaCirculo(r)
 print("{:.4f}".format(a))
