333939686782168e6c8a76e0b8f5dd3626242532
Carlos García <carlosesolrac93@gmail.com>
2022-11-11 11:25:34 -0600

    Update udi.py

--- py/udi.py
+++ py/udi.py
@@ -2,12 +2,16 @@
 import requests
 import json 
 
-url = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP68257/datos/2022-11-01/2022-11-02 displa "
+url = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP68257/datos/2022-11-01/2022-11-02"
 
 payload={}
 headers = {}
+parametres = {
+    "token": "04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471"
+}
 
-response = requests.get(url,verify=False)
+response = requests.get(url, params=parametres)
 
 text = json.dumps(response.text, sort_keys=True, indent=4)
+
 print(text)
