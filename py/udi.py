1067bfe630b7a3e93fa1ea2a638988ae6206ca39
Carlos García <carlosesolrac93@gmail.com>
2022-11-11 13:37:49 -0600

    Update udi.py

--- py/udi.py
+++ py/udi.py
@@ -1,6 +1,6 @@
 import http.client
 
-conn = http.client.HTTPSConnection("www.banxico.org.mx")
+conn = http.client.HTTPConnection("www.banxico.org.mx")
 payload = ''
 headers = {}
 conn.request("GET", "/SieAPIRest/service/v1/series/SP68257/datos?token=04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471", payload, headers)
