b9da7aceec1f4d9f297690121dec90f5d1309d6d
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2022-11-14 11:18:16 -0600

    Update udi.py

--- py/udi.py
+++ py/udi.py
@@ -1,7 +1,7 @@
 from pyodide.http import pyfetch
 import asyncio
 
-response = await pyfetch(url="https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP68257/datos?token=04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471", method="GET")
+response = await pyfetch(url="https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP68257/datos/2022-11-14/2022-11-14?token=04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471", method="GET")
 
 output = f"GET request=> status:{response.status}, json:{await response.json()}"
 
