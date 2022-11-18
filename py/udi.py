8f6e789b06efafc6e73b40e2d68fe9e7cae4f137
Carlos García <carlosesolrac93@gmail.com>
2022-11-18 13:44:14 -0600

    arrays

--- py/udi.py
+++ py/udi.py
@@ -1,5 +1,7 @@
 import json
 import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
 hoy=dt.date.today().strftime('%Y-%m-%d')
 
 url_api="https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP68257/datos/"+hoy+"/"+hoy+"?token=04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471"
@@ -18,5 +20,42 @@ data=json.loads(jsonsdata)
 valor=data['bmx']['series'][0]['datos'][0]['dato']
 fecha=data['bmx']['series'][0]['datos'][0]['fecha']
 
-pyscript.write('udi', valor) 
+pyscript.write('udi', valor)
 pyscript.write('fecha', fecha) 
+
+url_api1="https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP68257/datos/2022-08-21/"+hoy+"?token=04021aac739b77e232d9670147936836e9e9fc31e08bde26665c0f013df94471"
+
+response = await pyfetch(url=url_api1, method="GET")
+
+jsonsdatas1=await response.json()
+
+jsonsdatas1=json.dumps(jsonsdatas1)
+
+data1=json.loads(jsonsdatas1)
+
+datahistoric=data1['bmx']['series'][0]['datos']
+
+arr_data=np.array(datahistoric)
+
+x = np.arange(0, 100, 10, dtype=float)
+
+key = 0
+
+datos=np.array
+
+for i in arr_data:
+    datos[key] = i['dato']
+    key = key + 1
+    print(datos)
+
+# print(datos)
+# y = np.vstack([arr_data])
+
+# fig, ax = plt.subplots()
+
+# ax.set(xlim=(0, 100), xticks=np.arange(1, 100),
+# ylim=(0, 100), yticks=np.arange(1, 100))
+
+# ax.stackplot(x, y)
+
+# plt.show()
